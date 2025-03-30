9c7d069fd85af8f788bda237db7acdee2f18508d
Austin1serb <serbaustin@gmail.com>
2025-03-29 22:00:58 -0600

    added fix for issue #7751

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,36 @@
 import type { NextConfig } from "next";
+// import withBundleAnalyzer from "@next/bundle-analyzer"
 
+// const withAnalyzer = withBundleAnalyzer({ enabled: process.env.ANALYZE === "true" })
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-};
+	/* config options here */
 
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https" as const,
+				hostname: "lh3.googleusercontent.com",
+				port: "",
+				pathname: "/**",
+			},
+			{
+				protocol: "https" as const,
+				hostname: "res.cloudinary.com",
+				port: "",
+				pathname: "/**",
+			},
+		],
+	},
+};
 export default nextConfig;
+// export default withAnalyzer(nextConfig)
+
+// MDX
+// const withMDX = createMDX({
+//   extension: /\.mdx?$/,
+//   options: {
+//     remarkPlugins: [remarkGfm],
+//   },
+// })
+
+// export default withMDX(nextConfig)
